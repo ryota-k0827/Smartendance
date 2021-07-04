b4395f8b2e58caee5063bc4784521de90719e897
ryota-k0827 <50436249+ryota-k0827@users.noreply.github.com>
2021-07-04 22:37:36 +0900

    POSTからGETに一旦変更！

--- login.php
+++ login.php
@@ -1,53 +1,58 @@
 <?php
-    //swiftのログインボタンが押された時
-    //if(isset($_POST['login'])){
-        //swiftから送られてきた値
-        $userId = $_GET['userId'];
-        $password = $_GET['password'];
+    //swiftから送られてきた値
+    $userId = $_GET['userId'];
+    $password = $_GET['password'];
 
-        //空白チェック
-        if($userId != null && $password != null){   //入力されている場合
-            //ユーザIDが数値かどうか
-            if(preg_match('/^[0-9]+$/', $userId)){
-                $sql = "SELECT * FROM students WHERE student_number = ".$userId." AND password = '".$password."'";
-            }else{
-                $sql = "SELECT * FROM instructors WHERE roma_name = '".$userId."' AND password = '".$password."'";
+    //空白チェック
+    if($userId != null && $password != null){   //入力されている場合
+        //ユーザIDが数値かどうか
+        if(preg_match('/^[0-9]+$/', $userId)){
+            $sql = "SELECT * FROM students WHERE student_number = ".$userId." AND password = '".$password."'";
+        }else{
+            $sql = "SELECT * FROM instructors WHERE roma_name = '".$userId."' AND password = '".$password."'";
+        }
+        //sql
+        $link = mysqli_connect('localhost','root','','smartendance');
+        mysqli_set_charset($link,'utf8');
+        $data = mysqli_query($link,$sql);
+        mysqli_close($link);
+
+        $record = mysqli_fetch_assoc($data);    //sqlから取り出した値を$recordに格納
+
+        //入力されたuserIdとpasswordが一致したかどうか
+        if($record != null){    //一致した場合
+            //ユーザが学生か教官かどうか
+            if(preg_match('/^[0-9]+$/', $userId)){  //学生の場合
+                $user_data = array(
+                    'userId'=>$userId,
+                    'name'=>$record['name'],
+                    'type'=>'student',
+                    'classId'=>$record['classes_id']
+                );
+            }else{  //教官の場合
+                $user_data = array(
+                    'userId'=>$userId,
+                    'name'=>$record['name'],
+                    'type'=>'instructor',
+                    'classId'=>'null'
+                );
             }
-            //sql
-            $link = mysqli_connect('localhost','root','','smartendance');
-            mysqli_set_charset($link,'utf8');
-            $data = mysqli_query($link,$sql);
-            mysqli_close($link);
-
-            $record = mysqli_fetch_assoc($data);    //sqlから取り出した値を$recordに格納
-
-            //入力されたuserIdとpasswordが一致したかどうか
-            if($record != null){    //一致した場合
-                //ユーザが学生か教官かどうか
-                if(preg_match('/^[0-9]+$/', $userId)){  //学生の場合
-                    $user_data = array(
-                        'userId'=>$userId,
-                        'name'=>$record['name'],
-                        'type'=>'student',
-                        'classId'=>$record['classes_id']
-                    );
-                }else{  //教官の場合
-                    $user_data = array(
-                        'userId'=>$userId,
-                        'name'=>$record['name'],
-                        'type'=>'instructor',
-                        'classId'=>'null'
-                    );
-                }
-                echo json_encode($user_data, JSON_UNESCAPED_UNICODE);
-            }else{  //一致しなかった場合
-                echo "エラー";
-                return;
-            }
-        }else{  //入力されていない場合
-            echo "入力されていない";
+            echo json_encode($user_data, JSON_UNESCAPED_UNICODE);
+        }else{  //一致しなかった場合
+            $resultMsg = array(
+                'resultMsg'=>'ユーザIDまたはパスワードが正しくありません。'
+            );
+            echo json_encode($resultMsg, JSON_UNESCAPED_UNICODE);
+            //echo "エラー";
             return;
         }
-    //}
-
+    }else{  //入力されていない場合
+        $resultMsg = array(
+            'resultMsg'=>'入力されていません。'
+        );
+        echo json_encode($resultMsg, JSON_UNESCAPED_UNICODE);
+        //echo "入力されていない";
+        return;
+    }
     //require_once './tpl/login.php';
+?>
