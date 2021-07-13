1d427b05845c39136c60f0d14091ecaeb12c643d
KoudaiKanemaru <kanemaru.koudai@gmail.com>
2021-07-13 16:28:05 +0900

    add

--- attendance_confirmation.php
+++ attendance_confirmation.php
@@ -10,7 +10,7 @@
     //sql
     $link = mysqli_connect('localhost','root','','smartendance');
     mysqli_set_charset($link,'utf8');
-    $data = mysqli_query($link,"SELECT no,classes.class_symbol,time_id,class_times.start_time,class_times.end_time,subjects.subject,class_rooms.class_room
+    $data = mysqli_query($link,"SELECT *
                                 FROM teaches 
                                 INNER JOIN class_rooms 
                                 ON teaches.class_room_id = class_rooms.id 
@@ -22,71 +22,63 @@
                                 ON teaches.classes_id = classes.id
                                 INNER JOIN attend
                                 ON teaches.no = attend.no
-                                WHERE day_of_the_week = ".$day_of_the_week." 
-                                AND class_rooms.class_room = '".$class_room_number."' 
-                                AND '".$time."' <= class_times.end_time 
-                                LIMIT 1");
+                                WHERE class_room = ".$class_room_number."
+                                AND classes_id = '".$classes_id."'
+                                AND start_time <= '".$time."'
+                                AND '".$time."' <= end_time");
 
-    $record = mysqli_fetch_assoc($data);    //sqlから取り出した値を$recordに格納
+    $record = [];
+    $cnt = 0;
+    while($row = mysqli_fetch_assoc($data)){
+        $record[] = $row;
+        $cnt++;
+    }
 
     var_dump($record);
 
-    //現在時間で開講している授業があるか
-    if($record != null){    //ある場合
-        $no = $record['no'];
-        $data_1 = mysqli_query($link,"SELECT no,student_number,attend_day
-                                        FROM attend
-                                        WHERE no = ".$no."
-                                        AND student_number = ".$userId."
-                                        AND attend_day = '".$date."'");
-
-        $record_1 = mysqli_fetch_assoc($data_1);    //sqlから取り出した値を$record_1に格納
-        
-        if($record_1 == null){
-            //出席確認
-            if($time <= $record['start_time']){  //時間内に出席できた場合
-                //現在の日時を取得
-                $nowDate = date("Y/m/d H:i:s");
-                $data = mysqli_query($link,"INSERT INTO attend (no,student_number,attend_day,attend_time) VALUES (".$no.", ".$userId.",'".$date."','".$time."',1);");
-                $ary_data = array(
-                    'classRoomNuber'=>$class_room_number,
-                    'classSymbol'=>$record['class_symbol'],
-                    'subject'=>$record['subject'],
-                    'attendTime'=>$nowDate,
-                    'resultMsg'=>"",
-                    'status'=>"入室完了"
-                );
-                // 配列をjson_encode関数でJSON形式に変換します。
-                echo json_encode($ary_data, JSON_UNESCAPED_UNICODE);
-            }else{
-                if($time <= date('H:i:s',strtotime($record['start_time'].'+15 minute'))){    //15分以内の遅刻で出席した場合
-                    //現在の日時を取得
-                    $nowDate = date("Y/m/d H:i:s");
-                    $data = mysqli_query($link,"INSERT INTO attend (no,student_number,attend_day,attend_time) VALUES (".$no.", ".$userId.",'".$date."','".$time."',2);");
-                    $attend_data = array(
-                        'classRoomNuber'=>$class_room_number,
-                        'classSymbol'=>$record['class_symbol'],
-                        'subject'=>$record['subject'],
-                        'attendTime'=>$nowDate,
-                        'resultMsg'=>"（遅刻）",
-                        'status'=>"入室完了"
-                    );
-                }else{  //15分よりも後に出席した場合
-                    $nowDate = date("Y/m/d H:i:s");
-                    $data = mysqli_query($link,"INSERT INTO attend (no,student_number,attend_day,attend_time,status) VALUES (".$no.", ".$userId.",'".$date."','".$time."',3);");
-                    $attend_data = array(
-                        'classRoomNuber'=>$class_room_number,
-                        'classSymbol'=>$record['class_symbol'],
-                        'subject'=>$record['subject'],
-                        'attendTime'=>$nowDate,
-                        'resultMsg'=>'（授業開始後15分が経過した為、欠席です。）',
-                        'status'=>"入室完了"
-                    );
-                }
-                // 配列をjson_encode関数でJSON形式に変換します。
-                echo json_encode($attend_data, JSON_UNESCAPED_UNICODE);
-            }
-        }
+    $data1 = mysqli_query($link,"SELECT * FROM
+                                students 
+                                WHERE
+                                NOT EXISTS (
+                                    SELECT
+                                    *
+                                    FROM
+                                    attend
+                                    WHERE
+                                    students.student_number = attend.student_number
+                                )");
+    $record1 = [];
+    while($row = mysqli_fetch_assoc($data1)){
+        $record1[] = $row;
     }
+
+    $absenteeNumber = [];
+    $absenteeName = [];
+    $index = 0;
+    foreach($record1 as $val){
+        $absenteeNumber[] = $record1[$index]['attendance_number'];
+        $absenteeName[] = $record1[$index]['name'];
+        $index++;
+    }
+
+    $data2 = mysqli_query($link,"SELECT COUNT(*) FROM students WHERE classes_id =".$classes_id);
+    $record2 = [];
+    while($row = mysqli_fetch_assoc($data2)){
+        $record2[] = $row;
+    }
+
+    var_dump($record2);
+    $ary_data = array(
+        'classRoomNumber' => $class_room_number,
+        'classSymbol' => $record[0]['class_symbol'],
+        'subject' => $record[0]['subject'],
+        'number_of_attendees' => $cnt,
+        'class_size' =>  $record2[0]["COUNT(*)"],
+        'absenteeNumber' => $absenteeNumber,
+        'absenteeName' => $absenteeName
+    );
+
+    // 配列をjson_encode関数でJSON形式に変換します。
+    echo json_encode($ary_data, JSON_UNESCAPED_UNICODE);
     mysqli_close($link);
 ?>
